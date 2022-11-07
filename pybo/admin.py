552c1fdb9114167fa0317fc514a70bef303421a6
GGAE99 <duddn1529@gmail.com>
2022-11-07 15:41:44 +0900

    first commit in pybo project

---
+++ pybo/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Question
+from .models import Answer
+
+admin.site.register(Answer)
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+admin.site.register(Question, QuestionAdmin)
---
+++ pybo/views/base_views.py
@@ -0,0 +1,34 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+
+from ..models import Question
+from django.db.models import Q
+
+def index(request):
+    page = request.GET.get('page', '1') # 페이지
+    # kw 변수에 받아온 kw 값 추가 (검색어)
+    kw = request.GET.get('kw', '')  # 검색어
+    question_list = Question.objects.order_by('-create_date')
+    # kw 조건 검색
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    # 보내주는 값에 'page' 값과 'kw' 값 추가
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    return render(request, 'pybo/question_list.html', context)
+
+#def onana(request):
+#    return HttpResponse("This is Onana Page.")
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
